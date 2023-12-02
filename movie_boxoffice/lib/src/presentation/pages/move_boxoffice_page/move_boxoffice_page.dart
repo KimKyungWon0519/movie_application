3bbaa0268de8efe55e5be0b66e342fb4f210db6f
kim kyugnwon <gwon080870@gmail.com>
2023-12-02 20:51:39 +0900

    FEAT: Create Movie Card

--- movie_boxoffice/lib/src/presentation/pages/move_boxoffice_page/move_boxoffice_page.dart
+++ movie_boxoffice/lib/src/presentation/pages/move_boxoffice_page/move_boxoffice_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_boxoffice/src/presentation/pages/move_boxoffice_page/local_widgets/movie_card.dart';
 
 class MovieBoxofficePage extends StatelessWidget {
   const MovieBoxofficePage({super.key});
@@ -21,53 +22,7 @@ class MovieBoxofficePage extends StatelessWidget {
             Expanded(
               child: ListView.builder(
                 itemBuilder: (context, index) {
-                  return SizedBox(
-                    height: MediaQuery.sizeOf(context).height / 5,
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      clipBehavior: Clip.hardEdge,
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Image.network(
-                            'https://www.movieposters.com/cdn/shop/products/108b520c55e3c9760f77a06110d6a73b_e97cf224-d57f-44e3-8477-4f5479cd746b_480x.progressive.jpg?v=1573616089',
-                          ),
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.all(10),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    '영화 제목',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .titleSmall
-                                        ?.copyWith(fontWeight: FontWeight.bold),
-                                  ),
-                                  Expanded(
-                                    child: Text(
-                                      '영화 내용' * 100,
-                                      overflow: TextOverflow.fade,
-                                    ),
-                                  ),
-                                  Text(
-                                    '개봉일 : 2023-05-11',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodySmall
-                                        ?.copyWith(color: Colors.grey[800]),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
+                  return const MovieCard();
                 },
                 itemCount: 50,
               ),
