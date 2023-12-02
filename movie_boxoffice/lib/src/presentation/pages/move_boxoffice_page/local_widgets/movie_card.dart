db212a3f1af2a40d04a93bbbe8725e3ba108df19
kim kyugnwon <gwon080870@gmail.com>
2023-12-02 21:03:53 +0900

    FEAT: Create MoveInfoPanel

--- movie_boxoffice/lib/src/presentation/pages/move_boxoffice_page/local_widgets/movie_card.dart
+++ movie_boxoffice/lib/src/presentation/pages/move_boxoffice_page/local_widgets/movie_card.dart
@@ -12,38 +12,14 @@ class MovieCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(15),
         ),
         clipBehavior: Clip.hardEdge,
-        child: Row(
+        child: const Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const _ThumbNail(),
+            _ThumbNail(),
             Expanded(
               child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '영화 제목',
-                      style: Theme.of(context)
-                          .textTheme
-                          .titleSmall
-                          ?.copyWith(fontWeight: FontWeight.bold),
-                    ),
-                    Expanded(
-                      child: Text(
-                        '영화 내용' * 100,
-                        overflow: TextOverflow.fade,
-                      ),
-                    ),
-                    Text(
-                      '개봉일 : 2023-05-11',
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodySmall
-                          ?.copyWith(color: Colors.grey[800]),
-                    ),
-                  ],
-                ),
+                padding: EdgeInsets.all(10),
+                child: _MovieInfo(),
               ),
             ),
           ],
@@ -60,6 +36,39 @@ class _ThumbNail extends StatelessWidget {
   Widget build(BuildContext context) {
     return Image.network(
       'https://www.movieposters.com/cdn/shop/products/108b520c55e3c9760f77a06110d6a73b_e97cf224-d57f-44e3-8477-4f5479cd746b_480x.progressive.jpg?v=1573616089',
+    );
+  }
+}
+
+class _MovieInfo extends StatelessWidget {
+  const _MovieInfo();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '영화 제목',
+          style: Theme.of(context)
+              .textTheme
+              .titleSmall
+              ?.copyWith(fontWeight: FontWeight.bold),
+        ),
+        Expanded(
+          child: Text(
+            '영화 내용' * 100,
+            overflow: TextOverflow.fade,
+          ),
+        ),
+        Text(
+          '개봉일 : 2023-05-11',
+          style: Theme.of(context)
+              .textTheme
+              .bodySmall
+              ?.copyWith(color: Colors.grey[800]),
+        ),
+      ],
     );
   }
 }
