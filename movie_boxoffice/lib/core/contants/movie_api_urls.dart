e8d81e53ff0e7be746281952b8a4fe1da9c351df
kim kyugnwon <gwon080870@gmail.com>
2023-12-05 02:21:59 +0900

    CHORE: Add movie api base url

---
+++ movie_boxoffice/lib/core/contants/movie_api_urls.dart
@@ -0,0 +1,6 @@
+final class MovieApiUrls {
+  const MovieApiUrls._();
+
+  static const String baseUrl =
+      'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice';
+}
--- movie_boxoffice/lib/src/data/data_source/movie_client.dart
+++ movie_boxoffice/lib/src/data/data_source/movie_client.dart
@@ -1,9 +1,10 @@
 import 'package:dio/dio.dart';
+import 'package:movie_boxoffice/core/contants/movie_api_urls.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'movie_client.g.dart';
 
-@RestApi()
+@RestApi(baseUrl: MovieApiUrls.baseUrl)
 abstract class MovieClient {
   factory MovieClient(Dio dio, {String baseUrl}) = _MovieClient;
 }
